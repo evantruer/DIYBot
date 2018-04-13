caeb7c453e5b5db6f52417fc9ded13b2a6cef435
evantrue <evantrue@buffalo.edu>
2018-04-12 21:48:12 -0400

    Making the article into plaintext now works
    Ignoring the fact that *literally anything* can be posted in these
    articles which means no hard guarantees

--- pull_data.py
+++ pull_data.py
@@ -8,6 +8,7 @@ import re
 from html.parser import HTMLParser
 from enum import Enum
 import xml
+import string
 #import markov
 
 class textType(Enum):
@@ -26,6 +27,19 @@ def rand_wh_pages(n):
     return pages
 
 #cleans Wikitext from a string
+#After the old method of pulling data broke, I had to resort to pulling
+#from the API in a different format which preserved WikiText.
+#This is a major issue because there's no easy way to clean the text anymore.
+#I couldn't find documentation so there might be some things that aren't filtered
+#even though I've skimmed through dozens of exported WikiHow articles to find
+#the different forms WikiText generally takes on WikiHow.
+#
+#WikiHow itself isn't very forthcoming with API stuff.
+#The author of another bot asked WikiHow about said breaking API update.
+#Instead of fixing the issue, they sent him an angry message telling him to
+#shut down his bot. Since his bot was publishing their content and mine isn't,
+#there's no worry as far as legality is concerned but I'm not going to ask
+#WikiHow to fix their API.
 def clean_wikitext(s):
     if str(s) == "None" : return ""
     # remove hyperlinks
@@ -36,16 +50,120 @@ def clean_wikitext(s):
     s = re.sub("#\*", "", s)
     # remove #
     s = re.sub("#", "", s)
-    # remove whvids
-    s = re.sub("\{\{whvid.*\}\}", "", s)
-    #
-    # remove images
-    s = re.sub("\[\[Image:.*\]\]", "", s)
-    # replaces wikitext hyperlinks with link text
-    s = re.sub("\[\[.*\|", "", s)
-    s = re.sub("\]\]", "", s)
-    # remove any other [[]]
-    s = re.sub("\[\[.*\]\]", "", s)
+    #Cleaning/removal of things in double brackets
+    #We start by noting where [[, ]], {{, and }} are
+    curly_starts = []
+    curly_ends = []
+    square_starts = []
+    square_ends = []
+    #Note that for every start double bracket there is a corresponding and double bracket
+    #which occurs before any other double bracket
+    i = 1
+    while i < len(s):
+        if s[i - 1] == "{" and s[i] == "{": curly_starts.append(i - 1)
+        if s[i - 1] == "}" and s[i] == "}": curly_ends.append(i)
+        if s[i - 1] == "[" and s[i] == "[": square_starts.append(i - 1)
+        if s[i - 1] == "]" and s[i] == "]": square_ends.append(i)
+        i = i+1
+    #Now we decide what we need to replace the double brackets with
+    replacements = []
+    # Since what we want to do is dependent on the type of brace, we handle them separately
+    while len(curly_starts) > 0:
+        #Get the bounds of a pair of double braces
+        start = curly_starts.pop()
+        end = curly_ends.pop()
+        toReplace = s[start:end+1]
+        #Double brackets usually have "token1|token2|..." inside of them
+        tokens = str.split(toReplace[2:len(toReplace)-2], "|")
+        #There are a some things we just want to delete
+        #They generally mean things like "add a video/image"
+        #We know that if the first token starts with certain things, we want to move on
+        def should_ignore(token_list):
+            ignore_list = ["whvid", "video", "Video", "pictures", "Pictures", "stub", "Stub", "reflist", "fa", "intro", "Intro"]
+            for item in ignore_list:
+                if token_list[0].startswith(item):
+                    return True
+            return False
+        #If we should ignore, then we replace the brackets with nothing
+        if should_ignore(tokens):
+            replacements.append((toReplace, ""))
+        #otherwise, we need to look at it closer
+        else:
+            #If the first token is "convert", we need to replace it with "<number>" "<unit>":
+            if tokens[0] == "convert":
+                #a list of all units
+                length_abbr = "mm cm m km in ft mi "
+                length = "millimeter centimeter meter kilometer inch foot mile "
+                area_abbr = "cm2 m2 sqft ft2 sqmi mi2 "
+                area = "acre hectare "
+                mass_abbr = "g kg USoz lb "
+                mass = "gram kilogram ounce pound "
+                volume_abbr = "ml L USgal UStbsp USts "
+                volume = "millileter liter gallon tablespoon teaspoon "
+                speed_abbr = "mph kph "
+                speed = "milesperhour kilometersperhour "
+                time_abbr = "s min hr yr "
+                time = "second minute hour day month year "
+                units = str.split(length_abbr+length+area_abbr+area+mass_abbr+mass+volume_abbr+volume+speed_abbr+speed+time_abbr+time)
+                #defaults to 5 cm
+                chosenUnit = "cm"
+                chosenValue = "5"
+                for token in tokens:
+                    if token in units:
+                        chosenUnit = token
+                        break
+                for token in tokens:
+                    if any(char.isdigit() for char in token):
+                        chosenValue = token
+                        break
+                replacements.append((toReplace, chosenValue + " "+chosenUnit))
+            #if the first token is "keypress" we want to render as all tokens but the first separated by dashes
+            elif tokens[0] == "keypress" and len(tokens) > 1:
+                press = tokens[1]
+                for token in tokens[2:]:
+                    press = press + "-" + token
+                replacements.append((toReplace, press))
+            #If there are 2 tokens and it's not in any of the other cases it's usually some specially formatted text
+            #In this case, the second token is what a WikiHow user would see
+            elif len(tokens) == 2:
+                replacements.append((toReplace, tokens[1]))
+    while len(square_starts) > 0:
+        # Get the bounds of a pair of double braces
+        start = square_starts.pop()
+        end = square_ends.pop()
+        toReplace = s[start:end + 1]
+        # Double brackets usually have "token1|token2|..." inside of them
+        tokens = str.split(toReplace[2:len(toReplace) - 2], "|")
+
+        # There are a some things we just want to delete
+        # They generally mean things like "add a video/image"
+        # We know that if the first token starts with certain things, we want to move on
+        def should_ignore(token_list):
+            ignore_list = ["Category", "Image"]
+            for item in ignore_list:
+                if token_list[0].startswith(item):
+                    return True
+            return False
+
+        # If we should ignore, then we replace the brackets with nothing
+        if should_ignore(tokens):
+            replacements.append((toReplace, ""))
+        # otherwise, we need to look at it closer
+        else:
+            # If there are 2 tokens and it's not in any of the other cases it's usually some specially formatted text
+            # In this case, the second token is what a WikiHow user would see
+            if len(tokens) == 2:
+                replacements.append((toReplace, tokens[1]))
+            #if there's one token, we assume it should be printed
+            elif len(tokens) == 1:
+                replacements.append((toReplace, tokens[0]))
+    #We let regular expressions handle the rest
+    for replacement in replacements:
+        #print("replacing "+replacement[0]+ " with "+replacement[1])
+        #we have to escape replacement[0]
+        s = re.sub(re.escape(replacement[0]), replacement[1], s)
+    #remove multiple single quotation marks
+    s = re.sub("'{2,100}", "", s)
     return s.lstrip()
 
 #Takes the query result in XML format and parses it into a list of titles and steps in order
@@ -62,10 +180,10 @@ def parse_page(queryResult):
         #print("article found")
         #print(ET.tostring(article, method="xml", encoding="unicode"))
         title = "How To "+ article.find(namespace+"title").text
-        textList.append((textType.TITLE, title))
+        textList.append((textType.TITLE, title + "."))
         temp = article.find(namespace+"revision")
         text = temp.find(namespace+"text").text
-        #print("article text:\n"+text)
+        print("article text:\n"+text)
         linesList = text.splitlines()
         class states(Enum):
             ABSTRACT = 0
@@ -117,8 +235,9 @@ def findStartingWordsAndUrls(textList, ord = 1):
 
 result = rand_wh_pages(10)
 example = parse_page(result)
-#print("\n\ncleaned article:")
+print("\n\ncleaned article:")
 for line in example:
+    #break
     print (line[1])
     #if (("{{" in line or "}}" in line) or ("[[" in line or "]]" in line)) or ("<" in line or ">" in line): print("error in: "+line)
 
---
+++ script_legacy.py
@@ -0,0 +1,119 @@
+import pull_data
+import markov
+import random
+import facebook
+
+import time
+import sys
+
+order = 2
+articles = 5
+
+
+titles = 1
+abstracts = 0
+methods = 0
+summaries = 3
+steps = 0
+
+titles_sen = 1
+abstracts_sen = 3
+methods_sen = 1
+summaries_sen = 1
+steps_sen = 2
+
+titles_rec_size = 8
+abstracts_rec_size = 7
+methods_rec_size = 8
+summaries_rec_size = 13
+steps_rec_size = 5
+
+titles_min_words = 3
+abstracts_min_words = 7
+methods_min_words = 5
+summaries_min_words = 13
+steps_min_words = 5
+
+
+def makeGuide():
+    sourceList = pull_data.get_n_articles(articles)
+    tStarts, aStarts, mStarts, sStarts, xStarts, urls = pull_data.findStartingWordsAndUrls(sourceList, order)
+    text = pull_data.make_plaintext(sourceList)
+    tokens = markov.tokenize(text)
+    weights = markov.makeWeights(tokens, order)
+
+    artTitles = []
+    for item in sourceList:
+        if item[0] == pull_data.textType.TITLE: artTitles.append(item[1])
+
+    text = ''
+    for i in range(titles):
+        #print("title")
+        startWords = random.choice(tStarts)
+        numSentences = titles_sen
+        minWords = titles_min_words
+        text = text + markov.formatOutput(markov.makeText(weights, startWords, numSentences, True, order, titles_rec_size, titles_min_words)) + "\n"
+    #text = text +'\n'
+    for i in range(abstracts):
+        #print("abstract")
+        startWords = random.choice(aStarts)
+        numSentences = abstracts_sen
+        minWords = abstracts_min_words
+        text = text + markov.formatOutput(markov.makeText(weights, startWords, numSentences, True, order, abstracts_rec_size, abstracts_min_words)) + "\n"
+    #text = text +'\n'
+    for i in range(methods):
+        #print("method")
+        startWords = random.choice(mStarts)
+        numSentences = methods_sen
+        minWords = methods_min_words
+        text = text + markov.formatOutput(markov.makeText(weights, startWords, numSentences, True, order, methods_rec_size, methods_min_words)) + "\n"
+    text = text +'\n'
+    for i in range(summaries):
+        #print("summary")
+        startWords = random.choice(sStarts)
+        numSentences = summaries_sen
+        minWords = summaries_min_words
+        text = text + markov.formatOutput(markov.makeText(weights, startWords, numSentences, True, order, summaries_rec_size, summaries_min_words)) + "\n"
+    text = text +'\n'
+    for i in range(steps):
+        #print("step")
+        startWords = random.choice(xStarts)
+        numSentences = steps_sen
+        minWords = steps_min_words
+        text = text + markov.formatOutput(markov.makeText(weights, startWords, numSentences, True, order, steps_rec_size, steps_min_words)) + "\n"
+
+    return text, urls, artTitles
+
+def get_credentials(location='userData.ini'):
+    f = open('userData.ini', 'r')
+    items = f.read().split()
+    f.close()
+    page_id = ''
+    app_id = ''
+    app_secret = ''
+    access_token = ''
+    page_token = ''
+    for i in range(len(items)-1):
+        if items[i].lower() == 'page_id=': page_id = items[i+1]
+        elif items[i].lower() == 'app_id=': app_id = items[i+1]
+        elif items[i].lower() == 'app_secret=': app_secret = items[i + 1]
+        elif items[i].lower() == 'access_token=': access_token = items[i + 1]
+        elif items[i].lower() == 'page_token=': page_token = items[i + 1]
+
+    return page_id, app_id, app_secret, access_token, page_token
+
+page_id, app_id, app_secret, access_token, page_token = get_credentials()
+
+start = time.time()
+text, urls, titles = makeGuide()
+#print("access token: "+access_token)
+graph = facebook.GraphAPI(access_token= page_token)
+post_id = graph.put_object(parent_object=page_id, connection_name='feed', message= text)
+#print(post_id)
+commentStr = 'ARTICLES USED:'
+#for url in urls: commentStr = commentStr + "\n" + url
+for title in titles: commentStr = commentStr + "\n" + title
+commentStr = commentStr + "\n\n" + "(Links temporarily removed to avoid Facebook's spam filter)"
+graph.put_comment(object_id=post_id['id'], message=commentStr)
+print(commentStr)
+#print("time taken: "+ str(time.time()-start)+ "seconds")
