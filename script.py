09dd7f177d7e31703b94fc6e0c7692479baffd68
evantrue <evantrue@buffalo.edu>
2018-04-16 10:30:56 -0400

    Small changes to script

--- script.py
+++ script.py
@@ -7,20 +7,16 @@ import time
 import sys
 
 order = 2
-articles = 10
+articles = random.randrange(4, 8)
 
-
-titles = 1
-steps = 3
-
-titles_sen = 1
-steps_sen = 2
+steps = random.randrange(3, 5)
+steps_sen = 1
 
 titles_rec_size = 8
 steps_rec_size = 5
 
 titles_min_words = 3
-steps_min_words = 5
+steps_min_words = 3
 
 
 def makeGuide():
@@ -34,18 +30,18 @@ def makeGuide():
     for item in sourceList:
         if item[0] == pull_data.textType.TITLE: artTitles.append(item[1])
     text = ''
-    for i in range(titles):
-        #print("title")
-        startWords = random.choice(titleStarts)
-        numSentences = titles_sen
-        minWords = titles_min_words
-        text = text + markov.formatOutput(markov.makeText(weights, startWords, numSentences, True, order, titles_rec_size, titles_min_words)) + "\n"
+    #print("title")
+    startWords = random.choice(titleStarts)
+    numSentences = 1
+    minWords = titles_min_words
+    text = text + markov.formatOutput(markov.makeText(weights, startWords, numSentences, True, order, titles_rec_size, titles_min_words))
+    text = text[0:len(text)-1]+ ":\n"
     for i in range(steps):
         #print("summary")
         startWords = random.choice(stepStarts)
         numSentences = steps_sen
         minWords = steps_min_words
-        text = text + markov.formatOutput(markov.makeText(weights, startWords, numSentences, True, order, steps_rec_size, steps_min_words)) + "\n"
+        text = text + str(i+1) + ": " + markov.formatOutput(markov.makeText(weights, startWords, numSentences, True, order, steps_rec_size, steps_min_words)) + "\n"
     text = text +'\n'
 
     return text, artTitles
@@ -74,13 +70,16 @@ start = time.time()
 text, titles = makeGuide()
 #print("access token: "+access_token)
 graph = facebook.GraphAPI(access_token= page_token)
-#post_id = graph.put_object(parent_object=page_id, connection_name='feed', message= text)
+print("graph")
+#graph.request("0")
+post_id = graph.put_object(parent_object=page_id, connection_name='feed', message= text)
+print("post_id")
 #print(post_id)
 print(text)
 commentStr = 'ARTICLES USED:'
 #for url in urls: commentStr = commentStr + "\n" + url
-for title in titles: commentStr = commentStr + "\n" + title
+for title in titles: commentStr = commentStr + "\n" + title[0:len(title)-1]
 #commentStr = commentStr + "\n\n" + "(Links temporarily removed to avoid Facebook's spam filter)"
-#graph.put_comment(object_id=post_id['id'], message=commentStr)
+graph.put_comment(object_id=post_id['id'], message=commentStr)
 print(commentStr)
 #print("time taken: "+ str(time.time()-start)+ "seconds")
